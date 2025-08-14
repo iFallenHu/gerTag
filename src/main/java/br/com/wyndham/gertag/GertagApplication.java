bbe3df94c2bc6d6c51dcb89721ee1166341521d4
ti <ti@gnb.local>
2025-08-13 21:33:50 -0300

    Primeiro commit

---
+++ src/main/java/br/com/wyndham/gertag/GertagApplication.java
@@ -0,0 +1,13 @@
+package br.com.wyndham.gertag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GertagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GertagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wyndham/gertag/GertagApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wyndham.gertag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GertagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
